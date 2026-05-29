7953f6dacdce9253ce563d43541b2ad1fa6f8839
Viperssia <vadimevs06@mail.ru>
2026-05-29 17:51:31 +0700

    comit

---
+++ lab5/matrix.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void xmatrix (int **A, int **B, int **C, int N, int first_ind, int second_ind) {
+    for (int i = first_ind; i < second_ind; i++) {
+        for (int j = 0; j < N; j++) {
+            
+        }
+    }
+}
+
+int main() {
+
+
+    return 0;
+}
